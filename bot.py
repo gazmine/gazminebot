9ae69a4c8f2c08d59f0496ea1ba59c1bfe0ef634
Gazmine <gazmine.id@gmail.com>
2025-10-26 23:16:48 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import json
+import time
+import threading
+import os
+from decimal import Decimal, ROUND_HALF_UP
+from telebot import types
+
+# ========== CONFIG ==========
+TOKEN = "8307047195:AAGu-0bLJMKXZjDGZnuue_97WWaGZHz1-_E"
+bot = telebot.TeleBot(TOKEN)
+...
